259c9eab5ef13c42d251f8f5cec0638d084897ab
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-01-02 01:13:11 +0700

    scroll to bottom after 1.5s

--- src/app/components/public-chat/public-chat.component.ts
+++ src/app/components/public-chat/public-chat.component.ts
@@ -21,13 +21,26 @@ export class PublicChatComponent implements OnInit {
   ngOnInit(): void {
     this.allPublicChatService.watch().valueChanges.subscribe((data) => {
       this.publicMessages = [...this.publicMessages, ...data.data.messages];
+
+      this.scrollChatToBottom();
     });
 
     this.publicChatSubscriptionService.subscribe().subscribe((data) => {
       const publicMessage = data.data?.messageAdded;
       if (publicMessage) {
         this.publicMessages.push(publicMessage);
+
+        this.scrollChatToBottom();
       }
     });
+  }
+
+  scrollChatToBottom(): void {
+    setTimeout(() => {
+      const chatContainer = document.querySelector('.chat-container');
+      if (chatContainer) {
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+      }
+    }, 1500);
   }
 }
